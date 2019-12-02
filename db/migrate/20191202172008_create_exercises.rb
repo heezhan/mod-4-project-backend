74bee58ca70845421bc548bbf433fbf404f6a52e
heezhan <hhan1194@gmail.com>
2019-12-02 13:54:36 -0500

    created models and migrations

---
+++ db/migrate/20191202172008_create_exercises.rb
@@ -0,0 +1,12 @@
+class CreateExercises < ActiveRecord::Migration[6.0]
+  def change
+    create_table :exercises do |t|
+      t.string :name
+      t.string :video_url
+      t.string :type
+      t.string :muscle_group
+
+      t.timestamps
+    end
+  end
+end
