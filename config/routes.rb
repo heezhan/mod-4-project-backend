599e0f44f9dc0da9876640d063c275aecaf93c6c
Graham Flaspoehler <grahamflas@gmail.com>
2019-12-04 10:44:16 -0500

    Create controllers, draw routes, write controller actions that create new Routine and new RoutineExercises

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :exercises, only: [:index]
+  resources :routines, only: [:create]
+  resources :routine_exercises, only: [:create]
 end
