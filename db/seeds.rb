5a91df02a1222a3ac7288c1b425f7a0173645c13
Graham Flaspoehler <grahamflas@gmail.com>
2020-02-20 18:15:45 -0500

    Update seeds, replace URL of videos not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,14 @@
 graham = User.create(username: "graham", password: "password")
 heejae = User.create(username: "heejae", password: "password")
 
-r1 = Routine.create(title: "Monday Workout", user: graham)
-r2 = Routine.create(title: "Tuesday Workout", user: graham)
-r3 = Routine.create(title: "Monday Workout", user: heejae)
-r4 = Routine.create(title: "Tuesday Workout", user: heejae)
+# r1 = Routine.create(title: "Monday Workout", user: graham)
+# r2 = Routine.create(title: "Tuesday Workout", user: graham)
+# r3 = Routine.create(title: "Monday Workout", user: heejae)
+# r4 = Routine.create(title: "Tuesday Workout", user: heejae)
 
 #ABS
-ex1 = Exercise.create(name: "Ten Minute Ab Workout", video_url: "https://www.youtube.com/embed/cbCt69XAUKA&t=31s", exercise_type: "Body Weight", muscle_group: "Abs")
-Exercise.create(name: "Hourglass Ab Workout", video_url: "https://www.youtube.com/embed/k4Y37-Rsv4o&t=276s", exercise_type: "Body Weight", muscle_group: "Abs")
+ex1 = Exercise.create(name: "Ten Minute Ab Workout", video_url: "https://www.youtube.com/embed/cbCt69XAUKA", exercise_type: "Body Weight", muscle_group: "Abs")
+Exercise.create(name: "6 PACK ABS For Beginners", video_url: "https://www.youtube.com/embed/3p8EBPVZ2Iw", exercise_type: "Body Weight", muscle_group: "Abs")
 Exercise.create(name: "Six Pack Workout", video_url: "https://www.youtube.com/embed/Q-vuR4PJh2c", exercise_type: "Body Weight", muscle_group: "Abs")
 
 #CHEST
@@ -57,11 +57,11 @@ ex8 = Exercise.create(name: "Dips", video_url: "https://www.youtube.com/embed/2z
 Exercise.create(name: "Skull Crusher", video_url: "https://www.youtube.com/embed/BdqzYZmL2Bc", exercise_type: "Weight Lifting", muscle_group: "Triceps")
 Exercise.create(name: "Tricep Dumbbell Extension", video_url: "https://www.youtube.com/embed/nRiJVZDpdL0", exercise_type: "Weight Lifting", muscle_group: "Triceps")
 
-RoutineExercise.create(routine: r1, exercise: ex1)
-RoutineExercise.create(routine: r1, exercise: ex2)
-RoutineExercise.create(routine: r2, exercise: ex3)
-RoutineExercise.create(routine: r2, exercise: ex4)
-RoutineExercise.create(routine: r3, exercise: ex5)
-RoutineExercise.create(routine: r3, exercise: ex6)
-RoutineExercise.create(routine: r4, exercise: ex7)
-RoutineExercise.create(routine: r4, exercise: ex8)
+# RoutineExercise.create(routine: r1, exercise: ex1)
+# RoutineExercise.create(routine: r1, exercise: ex2)
+# RoutineExercise.create(routine: r2, exercise: ex3)
+# RoutineExercise.create(routine: r2, exercise: ex4)
+# RoutineExercise.create(routine: r3, exercise: ex5)
+# RoutineExercise.create(routine: r3, exercise: ex6)
+# RoutineExercise.create(routine: r4, exercise: ex7)
+# RoutineExercise.create(routine: r4, exercise: ex8)
